ff949ceb026152a2529fd7a78fc6caccaedb1769
Leith Essid <essidleith@gmail.com>
2022-05-23 17:48:05 +0200

    my seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,39 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+require "faker"
+
+Listing.destroy_all
+User.destroy_all
+
+User.create(
+  email: "admin@finest.com",
+  name: "admin",
+  password: "123456"
+)
+
+jess = User.create(
+  email: "advertiser@finest.com",
+  name: "L'armoire de Jess",
+  password: "123456"
+)
+
+User.create(
+  email: "renter@finest.com",
+  name: "Les Studios de Julien",
+  password: "123456"
+)
+
+15.times do
+  temp = rand(0..6)
+  Listing.create(
+    category: temp,
+    user_id: jess.id,
+    style: rand(0..8),
+    description: Faker::Lorem.sentences(number: 2),
+    deliverable: (rand <= 0.5),
+    price_per_day: rand(100..1000),
+    city: Faker::Address.city,
+    product_image: "https://source.unsplash.com/random/720%C3%97720/?#{temp}"
+  )
+end
