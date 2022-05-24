bcc61613b9f1d251193721c633f9ab8c818c9fbe
Leith Essid <essidleith@gmail.com>
2022-05-25 01:03:13 +0200

    Changed design and added some components

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,3 +2,12 @@
 import "@hotwired/turbo-rails"
 import "./controllers"
 import "bootstrap"
+
+
+const clearInput = () => {
+  const input = document.getElementsByTagName("input")[0];
+  input.value = "";
+}
+
+const clearBtn = document.getElementById("clear-btn");
+clearBtn.addEventListener("click", clearInput);
