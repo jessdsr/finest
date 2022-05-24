27e3caee0c278fadedf898ab4483f54d83b9c837
Gauthier Salavert <gauthiersalavert@gmail.com>
2022-05-24 13:09:53 +0200

    created the viezs for the listing class and the routes

--- app/policies/listing_policy.rb
+++ app/policies/listing_policy.rb
@@ -1,11 +1,22 @@
 class ListingPolicy < ApplicationPolicy
   class Scope < Scope
     # NOTE: Be explicit about which records you allow access to!
-    # def resolve
-    #   scope.all
-    # end
-    def show?
-      true
+    # Uncomment this 'resolve' action to see all the index of listings
+    # In another situation we would have return the index w/ Listings.all
+    def resolve
+      scope.all
     end
+  end
+
+  def show?
+    true
+  end
+
+  def new?
+    create?
+  end
+
+  def create?
+    true
   end
 end
