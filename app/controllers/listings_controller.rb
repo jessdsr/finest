a05bc15774c4b0c7d04376e4790d64efe6dab341
Julien Brieallard <julien.brieallard@gmail.com>
2022-05-26 17:43:36 +0200

    map ok

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -13,12 +13,12 @@ class ListingsController < ApplicationController
   def show
     authorize @listing
 
-    @marker = @listing.geocoded.map do |f|
+    @markers = [
       {
-        lat: f.latitude,
-        lng: f.longitude
+        lat: @listing.latitude,
+        lng: @listing.longitude
       }
-    end
+    ]
   end
 
   # GET /listings/new
