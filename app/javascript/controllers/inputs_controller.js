3e424ca1045306ec497cdbefa1b33c4179e0065f
Leith Essid <essidleith@gmail.com>
2022-05-27 16:51:31 +0200

    JS and forms changes

--- app/javascript/controllers/inputs_controller.js
+++ app/javascript/controllers/inputs_controller.js
@@ -5,16 +5,21 @@ export default class extends Controller {
   static targets = ['input', 'label']
   connect() {
     this.allInputs = document.querySelectorAll('.special-input')
+    console.log(this.labelTargets)
   }
 
   toggleInput(event) {
-    this.allInputs.forEach((input) => {
+    this.allInputs.forEach((input, index) => {
 
-        input.classList.add('hidden');
+      input.classList.add('hidden');
 
-      this.inputTarget.classList.remove('hidden')
+      this.inputTarget.classList.remove('hidden');
+      this.inputTarget.classList.remove('is-valid');
     })
+    this.labelTarget.innerHTML = this.inputTarget.value;
+  }
 
-
+  update() {
+    this.labelTarget.innerHTML = this.inputTarget.value;
   }
 }
